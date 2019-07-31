795a041b7f92d8a4573685e4a34a0e7385fae049
Ernesto <ERPEFE2006@gmail.com>
2019-07-31 20:18:19 -0300

    Sube y Baja.

---
+++ Agarrar.java
@@ -0,0 +1,11 @@
+import lejos.nxt.Motor;
+import lejos.util.Delay;
+
+public class Agarrar
+{
+    public static void main (String[] args)
+    {
+        Motor.A.backward();
+        msDelay(1000);
+    }
+}
---
+++ Depositar.java
@@ -0,0 +1,11 @@
+import lejos.nxt.Motor;
+import lejos.util.Delay;
+
+public class Depositar
+{
+    public static void main (String[] args)
+    {
+        Motor.A.forward();
+        msDelay(1000);
+    }
+}
