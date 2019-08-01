2c340c2e3fa726eb6abebe4ba657d2b7ee1d95a4
Ernesto <ERPEFE2006@gmail.com>
2019-07-31 21:31:41 -0300

    Ajustando Sube y Baja.

--- Depositar.java
+++ Depositar.java
@@ -3,9 +3,14 @@ import lejos.util.Delay;
 
 public class Depositar
 {
-    public static void main (String[] args)
+    public Depositar() {}
+
+    public void depositar()
     {
-        Motor.A.forward();
-        Delay.msDelay(1000);
+      System.out.println("Esto baja");
+      //Motor.B.setSpeed(100);
+      Motor.B.forward();
+      Delay.msDelay(200);
+	    Motor.B.stop();
     }
 }
--- Outlet_Connect.java
+++ Outlet_Connect.java
@@ -1,13 +1,20 @@
 import lejos.nxt.*;
+import lejos.util.Delay;
 
 public class Outlet_Connect {
 	//private static BTConnectTest2 pigFactory;
 	private static Agarrar aga;
- 
+	private static Depositar dep;
+
     public static void main(String[] args) {
     	//pigFactory = new BTConnectTest2();
         //pigFactory.start();
 	aga = new Agarrar();
 	aga.agarrar();
+	System.out.println("fin de agarrar");
+	//Delay.msDelay(5000);
+	dep = new Depositar();
+	dep.depositar();
+	Button.waitForAnyPress();
     }
 }
